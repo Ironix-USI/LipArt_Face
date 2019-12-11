87eddca88c4395a219283986abaed72a02f3c561
ililcheva <iilcheva@centroida.co>
2019-12-11 16:58:08 +0100

    Connected db to profile view for posts

--- app/src/main/java/com/mobile/lipart/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/mobile/lipart/ui/profile/ProfileFragment.java
@@ -1,9 +1,11 @@
 package com.mobile.lipart.ui.profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +13,14 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mobile.lipart.R;
+import com.mobile.lipart.model.User;
 
 public class ProfileFragment extends Fragment {
 
@@ -22,12 +31,13 @@ public class ProfileFragment extends Fragment {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-//        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(this, new Observer<String>() {
+        final TextView countTextView = root.findViewById(R.id.profile_post_count);
+        profileViewModel.getPostCount().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+                countTextView.setText(s);
             }
+
         });
         return root;
     }
--- app/src/main/java/com/mobile/lipart/ui/profile/ProfileViewModel.java
+++ app/src/main/java/com/mobile/lipart/ui/profile/ProfileViewModel.java
@@ -1,19 +1,46 @@
 package com.mobile.lipart.ui.profile;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.mobile.lipart.R;
+
 public class ProfileViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<String> mPostCount;
+    private DatabaseReference mDatabase;
+
+
 
     public ProfileViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is profile fragment");
+        mPostCount = new MutableLiveData<>();
+
+        String uId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("user-posts/" + uId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                mPostCount.setValue(String.valueOf(dataSnapshot.getChildrenCount()));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getPostCount() {
+        return mPostCount;
     }
 }
