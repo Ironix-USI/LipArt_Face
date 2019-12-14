a5023c210990943d565906697b0a7ddf9a274019
ililcheva <iilcheva@centroida.co>
2019-12-14 22:52:14 +0100

    Added toast on pressing Send in Feedback Activity

--- app/src/main/java/com/mobile/lipart/ui/profile/activities/FeedbackActivity.java
+++ app/src/main/java/com/mobile/lipart/ui/profile/activities/FeedbackActivity.java
@@ -1,6 +1,10 @@
 package com.mobile.lipart.ui.profile.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +17,15 @@ public class FeedbackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setTitle("Feedback");
         setContentView(R.layout.activity_feedback);
+
+        Button button = findViewById(R.id.button_send_feedback);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Thank you for your feedback!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
